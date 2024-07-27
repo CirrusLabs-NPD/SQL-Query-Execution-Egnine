cf6cb843713823f35c58b988a7a21196f263bc6a
CL-Raghav <raghav.agarwalla@cirruslabs.io>
2024-07-27 23:14:14 +0530

    FE delete from BE

--- sql-engine-test/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { FileUpload } from "./Cards/FileUpload";
-export { QueryAns } from "./Cards/QueryAns";
-export { EpicGeneration } from "./Cards/EpicGeneration";
-export { UserStoryGen } from "./Cards/UserStoryGen";
-export { Submit } from "./Cards/Submit";
